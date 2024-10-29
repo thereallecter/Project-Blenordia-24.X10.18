df9c5947bf7533d745da83cee2aa2b948fd3e8e9
thereallecter <cthereallecter@gmail.com>
2024-10-28 19:22:48 -0700

    Create PirateCave.cs

---
+++ Core/Source/World/Maps/PirateCave/PirateCave.cs
@@ -0,0 +1,12 @@
+namespace Blenordia.Maps 
+{
+    public class PirateCave 
+    {
+        public MapInfo Info;
+
+        public PirateCave(MapInfo info) 
+        {
+            Info = info;
+        }
+    }
+}
